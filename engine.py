3b07167986aa327b0c1858cc24d06be58577ea9d
itsspher <43953524+itsspher@users.noreply.github.com>
2018-10-09 13:43:51 +0800

    defined function dictionaryinput
    dictionaryinput takes in an argument filename, loads the list of words found in filename as a list in the program

--- engine.py
+++ engine.py
@@ -1,1 +1,7 @@
 
+def dictionaryinput(filename):
+  file_input = open(filename, 'r')
+  dictionary_list = list()
+  for line in file_input:
+    line = line.split()
+    dictionary_list.append(line)
