8523d62c781360b1d6e1f613e6a23a8e2360a944
itsspher <43953524+itsspher@users.noreply.github.com>
2018-10-09 13:57:15 +0800

    defined calcscore
    calcscore takes an argument word and returns the equivalent score of the word based on individual letter values

--- engine.py
+++ engine.py
@@ -5,3 +5,23 @@ def dictionaryinput(filename):
   for line in file_input:
     line = line.split()
     dictionary_list.append(line)
+
+
+def calcscore(word):
+	
+	score = 0
+	
+	scores = {
+	'e':1, 'a':1, 'i':1, 'o':1, 'u':1, 'n':1, 'r':1, 't':1, 'l':1, 's':1,
+	'd':2, 'g':2,
+	'b':3, 'c':3, 'm':3, 'p':3,
+	'f':4, 'h':4, 'v':4, 'w':4, 'y':4,
+	'k':5,
+	'j':8, 'x':8, 
+	'q':10, 'z':10
+	}
+	
+	for letter in word:
+		score += scores[letter]
+		
+	return score
