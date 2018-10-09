9a6fb9413ae7a321163373628968b7d628041f39
Nicole <41113514+nicolemadison@users.noreply.github.com>
2018-10-10 02:04:58 +0800

    added req 4 and 5 from sophia
    edited variables for readability:
    all lists have this format:  "variable_list"
    uses lowercase snake case e.g. "attribute_variable"
    added comments for each return statement

--- engine.py
+++ engine.py
@@ -1,13 +1,103 @@
-
+######REQ1######
 def dictionary_input(filename):
-  file_input = open(filename, 'r')
-  dictionary_list = list()
-  for line in file_input:
-    line = line.split()
-    dictionary_list.append(line)
+    file_input = open(filename, 'r')
+    dictionary_list = list()
+    for line in file_input:
+        line = line.split()
+        dictionary_list.append(line)
+    file_input.close()
+    return dictionary_list	
 
+#------------------------------------------------------------------------------------------------------------------------------#
 
-def calcscore(word):
+######REQ2######
+def picking_words():
+    t = int(input())
+	for i in range(t):
+	    position = input()
+	    position_list = [int(x) for x in position.split()]
+	   
+	    output_list = list()
+	    for j in range (len(position_list)):
+		index = position_list[j]                ##GETTING INDEX OF NUMBER FROM position_list
+		result = dictionary_list[index]         ##GETTING ENTRY FROM dictionary_list
+		output_list += result.split()
+
+	    wordlist = ""
+	    for word in output_list:
+		wordlist += word + " "
+    return wordlist.rstrip()				##RETURNS WORDS FROM dictionary_list FROM GIVEN STRING OF POSITION NUMBERS
+
+#------------------------------------------------------------------------------------------------------------------------------#
+
+######REQ3######	"MODE1"
+def search_anagram():
+    t = int(input())
+    for i in range(t):
+        query = input()
+        query = sorted(query)
+        query = "".join(query)
+            
+        output_list = []    
+        if query in reference:
+            indices = [i for i, x in enumerate(reference) if x == query]
+            
+            for index in indices:
+                result = dictionary_list[index]         ##GETTING ENTRY FROM DICTIONARY LIST 
+                output_list += result.split()
+               
+            wordlist = ""
+            for word in output_list:
+                wordlist += word + " "
+    return wordlist.rstrip()				##RETURNS ALL VALID ANAGRAMS FROM dictionary_list GIVEN ONE WORD
+
+#------------------------------------------------------------------------------------------------------------------------------#
+
+######REQ4######	"MODE2"
+def combine_words():
+    t = int(input())
+        for i in range (t):
+	    words = input()
+	    words = words.split()  
+	    game_list = list()
+
+	    for j in words:
+		for k in j:
+		    count_word = j.count(k)
+		    count_comb = game_list.count(k)
+		    if count_word > count_comb:
+			for l in range (count_word - count_comb):
+			    game_list.append(k)
+	    game_list = "".join(sorted(game_list))
+	    return game_list				##RETURNS SET OF SCRAMBLED LETTERS GIVEN STRING OF WORDS
+            
+#------------------------------------------------------------------------------------------------------------------------------#
+
+######REQ5######
+def check_words():
+    t = int(input())
+	for i in range (t):
+	    words = input()
+	    words = words.split() 
+	    scrambled, basis = words[0], words[1]
+    	    basis = basis.lower()
+            outcome = False
+
+	    dictionary_input(filename)   		###CALLING REQ1 FUNCTION
+					  		###THIS MUST RETURN THE dictionary_list
+	    for line in dictionary_list:
+		if basis == line.rstrip():
+            	    outcome = True
+	    
+	    for j in word:
+		if scrambled.count(j) > basis.count(j):
+		    outcome = False
+	    return outcome				##RETURNS OUTCOME EITHER TRUE / FALSE
+
+#------------------------------------------------------------------------------------------------------------------------------#	
+
+######REQ6######
+def calc_score(word):
 	
 	score = 0
 	
@@ -25,45 +115,3 @@ def calcscore(word):
 		score += scores[letter]
 		
 	return score
-
-def picking_words()
-    t = int(input())
-	for i in range(t):
-	    position = input()
-
-
-	    position_list = [int(x) for x in position.split()]
-	    #print("position list: " + str(position_list))
-
-	    output_list = []
-	    for j in range (len(position_list)):
-		index = position_list[j]                ## GETTING INDEX OF NUMBER FROM POSITION LIST
-		result = dictionary_list[index]         ## GETTING ENTRY FROM DICTIONARY LIST 
-		output_list += result.rsplit()
-
-	    wordlist = ""
-	    for word in output_list:
-		wordlist += word + " "
-    return wordlist.rstrip()
-
-
-
-def search_anagram()
-    t = int(input())
-    for i in range(t):
-        query = input()
-        query = sorted(query)
-        query = "".join(query)
-            
-        output_list = []    
-        if query in reference:
-            indices = [i for i, x in enumerate(reference) if x == query]
-            
-            for index in indices:
-                result = dictionary_list[index]         ## GETTING ENTRY FROM DICTIONARY LIST 
-                output_list += result.rsplit()
-               
-            wordlist = ""
-            for word in output_list:
-                wordlist += word + " "
-    return wordlist.rstrip()
